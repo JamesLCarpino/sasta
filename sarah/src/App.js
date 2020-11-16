40e70d94fa93c87c73bc2725d39fd77d5eceedde
Pinefoot <jameslcarpino@gmail.com>
2020-11-16 15:54:28 -0800

    sasta page renamed and working

--- sarah/src/App.js
+++ sarah/src/App.js
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
-
 import "./App.scss";
 import LandingPage from "./components/common/LandingPage";
+import Register from "./components/auth/Register";
+import { Route } from "react-router-dom";
 
+const routes = [
+  {
+    path: "/",
+    name: "Landing",
+    Component: LandingPage,
+  },
+  { path: "/register", name: "Register", Component: Register },
+];
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div className="App">
-      {/* {loggedIn === false ? (
-                <Login></Login>
-            ): (
-                
-            )} */}
-      <LandingPage />
+      {routes.map(({ path, Component }) => (
+        <Route key="name" path={path} exact>
+          <Component />
+        </Route>
+      ))}
+
+      {/* <Route path="/">
+        <LandingPage />
+      </Route>
+      <LandingPage /> */}
     </div>
   );
 }
---
+++ sarah/src/components/auth/Register.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Register() {
+  return <h1 style={{ fontColor: "white" }}>REGISTER PAGE</h1>;
+}
