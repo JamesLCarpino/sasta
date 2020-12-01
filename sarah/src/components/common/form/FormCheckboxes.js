2bbac6bfcb92ea6b7059d4f1ff2b036851d985aa
Pinefoot <jameslcarpino@gmail.com>
2020-11-30 16:40:04 -0800

    pushing changes up working on checkbox section.

--- sarah/src/components/common/form/FormCheckboxes.js
+++ sarah/src/components/common/form/FormCheckboxes.js
@@ -1,39 +1,61 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
 import updateAction from "./updateAction";
-import { useStateMachine } from "little-state-machine";
+import {
+  enterBottomNeeds,
+  enterTopNeeds,
+  enterFootwareNeeds,
+  enterAccessoriesNeeds,
+  enterCoats_JacketsNeeds,
+  enterDresses_JumpSuitsNeeds,
+  enterColorSubNeeds,
+  enterByOutfitNeeds,
+} from "../../../state/rootSlice";
 
 export default function FormCheckboxes(setFormStep) {
-  const { state, action } = useStateMachine(updateAction);
-  const { handleSubmit, errors } = useForm({
-    defaultValues: state.details,
+  const dispatch = useDispatch();
+  const history = useHistory();
+  //all checkbox fields
+  const bottoms = useSelector((state) => state.needs.bottoms);
+  const tops = useSelector((state) => state.needs.tops);
+  const coats_jackets = useSelector((state) => state.needs.coats_jackets);
+  const dresses_jumpsuits = useSelector(
+    (state) => state.needs.dresses_jumpsuits
+  );
+  const footwear = useSelector((state) => state.needs.footwear);
+  const accessories = useSelector((state) => state.needs.accessories);
+  const by_outfit = useSelector((state) => state.needs.by_outfit);
+  const color_sub = useSelector((state) => state.needs.color_sub);
+
+  const { handleSubmit, errors, register } = useForm({
+    defaultValues: {
+      bottoms,
+      tops,
+      coats_jackets,
+      dresses_jumpsuits,
+      footwear,
+      accessories,
+      by_outfit,
+      color_sub,
+    },
   });
+  console.log("finding out what state is from checkbox", bottoms);
 
   const onSubmit = (data) => {
-    //action(data);
-    // setFormStep({
-    //   formCheckboxes: false,
-    // });
-    console.log("Checkboxes", data.target.value);
-    //
+    dispatch(enterBottomNeeds(data.bottoms));
+    console.log("Checkboxes", data.bottoms);
   };
 
-  const inputValue = (data) => {
-    action(data.target.checked);
-    console.log("cadat check ", data.target.value);
-  };
+  //
+
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <h2>Select your needs!</h2>
         <label>
-          Bottoms:
-          <input
-            name="bottoms"
-            type="checkbox"
-            onChange={inputValue}
-            value="bottoms"
-          />
+          <input name="shorts" type="checkbox" value="shorts" />
         </label>
         <button type="submit">submit</button>
       </form>
